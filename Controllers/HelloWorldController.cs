5cec4df97607abb8a51eb3118d5aaa7dba101620
Tyrone Turrell <tyturrell@gmail.com>
2021-09-01 15:20:16 -0400

    visual studio test

--- Controllers/HelloWorldController.cs
+++ Controllers/HelloWorldController.cs
@@ -3,14 +3,24 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using System.Text.Encodings.Web;
 
 namespace MvcMovie.Controllers
 {
     public class HelloWorldController : Controller
     {
-        public IActionResult Index()
+       //
+       // GET: /HelloWorld/
+       public string Index()
         {
-            return View();
+            return "This is my defualt action";
+        }
+
+        //
+        // GET: /HelloWorld/Welcome/
+        public string Welcome(string name, int ID = 1)
+        {
+            return HtmlEncoder.Default.Encode($"Hello {nameof}, ID: {ID}" ;
         }
     }
 }
